6eb6faacb5ff7480473a5ae1c26f85f2178154a6
Giuseppe Bottiglieri <giuseppe.bottiglieri@gmail.com>
2018-02-01 06:59:25 +0100

    Initial Commit

---
+++ RoversOnMars/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RoversOnMars
+//
+//  Created by Giuseppe Bottiglieri on 01/02/18.
+//  Copyright © 2018 gbtt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
